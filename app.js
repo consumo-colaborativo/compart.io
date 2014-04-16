0c607eb4453ee64e4afc6faefa9a47ccd035426e
magda <magdasanchez@colaborativa.eu>
2014-04-16 14:01:09 +0200

    Re-writting deploy.sh

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ compart.io Express Web Server
   var http = require('http');
   var stylus = require('stylus');
   var app = express();
+ /* MGD: Enabling Automatic Deployment */
+	app.post('/deploy/', function (req, res) {  
+	     var spawn = require('child_process').spawn,
+	        deploy = spawn('sh', ['./deploy.sh' ]);
+
+	    deploy.stdout.on('data', function (data) {
+	        console.log(''+data);
+	    });
+
+	    deploy.on('close', function (code) {
+	        console.log('Child process exited with code ' + code);
+	    });
+	    res.json(200, {message: 'Github Hook received!'})
+	});
 
 	app.configure(function(){
 		app.set('port', 80);
