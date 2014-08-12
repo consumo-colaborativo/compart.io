651b50136b07530f1a33e941ea092744e256b6d1
grhs <grhs@grayhats.eu>
2014-08-12 20:10:10 +0200

    creo el esquema para objeto compartio

---
+++ app/server/model/compartio.js
@@ -0,0 +1,35 @@
+// app/model/compartio.js
+
+var mongoose 	= require('mongoose');
+var bcrypt   	= require('bcrypt-nodejs');
+var ObjectId 	= mongoose.Schema.Types.ObjectId;
+var citySchema 	= require('./city');
+var userSchema	= require('./user');
+
+var compartioSchema = mongoose.Schema({
+
+	status			: { type: String, default: '' },
+	date_publish	: { type: Date, default: Date.now },
+	giver			: { type: ObjectId, ref: 'person' },
+	receiver		: { type: ObjectId, default: null, ref: 'person' },
+	i_like_it		: [{ type: ObjectId, ref: 'person' }],
+	i_want_it		: [{ type: ObjectId, ref: 'person' }],
+	name_of_item	: { type: String, default: ''},
+	desc_of_item	: { type: String, default: ''},
+	why_i_share_it	: { type: String, default: ''},
+	location		: {
+		type_address	: { type: String, default: '' },
+		street			: { type: String, default: '' },
+		city 			: { type: ObjectId, ref: 'city' },
+		zip_code		: { type: String, default: '' },
+		country			: { type: String, default: ''}
+	},
+	comments		: [{
+		user_that_comments	: { type: ObjectId, ref: 'person' },
+		i_like_it_comment	: [{ type: ObjectId, ref: 'person' }],
+		comment_text		: { type: String, default: ''}
+	}] 
+});
+
+var city 	= mongoose.model('city', citySchema);
+var person	= mongoose.model('person', userSchema);
