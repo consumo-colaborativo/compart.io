140aab7982a77bd243d234b0fcde2bd8b272b423
Álvaro Saco <alvarosaco@gmail.com>
2014-08-12 19:09:28 +0200

    Some lines updated

--- app/server/model/city.js
+++ app/server/model/city.js
@@ -7,14 +7,13 @@ var mongoose = require('mongoose'),
 
 // define the schema for our city model
 var citySchema = new Schema({
-
-	id 				: { type: String},
-    name            : { type: String, required: true},	
-    postal_code     : { type: String},
-    slug     		    : { type: String},
-    active          : { type: Boolean}, 
-    creation_date	  : { type: Date, default: Date.now },
-    country_id      : { type: String}
+  id 				: { type: String},
+  name            : { type: String, required: true},	
+  postal_code     : { type: String},
+  slug     		    : { type: String},
+  active          : { type: Boolean}, 
+  creation_date	  : { type: Date, default: Date.now },
+  country_id      : { type: String}
 });
 
 // create the model "City" for cities and expose it to our app
